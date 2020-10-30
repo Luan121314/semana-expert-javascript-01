9c1bd0a33a095e3112fe418009f84a7403e9dab3
luan.staner <luan.staner@gmail.com>
2020-10-30 19:33:39 -0300

    first commit

---
+++ aula02/public/bandersnatch/index.js
@@ -0,0 +1,21 @@
+const MANIFEST_URL = 'manifest.json';
+const localhost = ['127.0.0.1', 'localhost'];
+
+async function main() {
+    const isLocal = !!~localhost.indexOf(window.location.hostname);
+    console.log('isLocal', isLocal);
+    const manifestJSON = await (await fetch(MANIFEST_URL)).json()
+    const host = isLocal ? manifestJSON.localhost : manifestJSON.productionHost;
+
+    const videoComponent = new VideoComponent();
+    const network  = new Network({host});
+    const videoPlayer = new VideoMediaPlayer({
+        manifestJSON,
+        network
+    })
+    videoPlayer.initializeCodec();
+    videoComponent.initializePlayer();
+
+}
+
+window.onload = main
