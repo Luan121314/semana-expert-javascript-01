d954373582734ad0bc2e34bb3a6775d393ee35c5
luan.staner <luan.staner@gmail.com>
2020-10-31 16:16:41 -0300

    resolved bugn in the SourceBuffer

--- aula02/public/bandersnatch/src/videoPlayer.js
+++ aula02/public/bandersnatch/src/videoPlayer.js
@@ -1,75 +1,74 @@
-class VideoMediaPlayer{
-    constructor({manifestJSON, network}){
+class VideoMediaPlayer {
+    constructor({ manifestJSON, network }) {
         this.manifestJSON = manifestJSON
         this.network = network
-        this.videoElement = null;
-        this.sourceBuffer = null;
+
+        this.videoElement = null
+        this.sourceBuffer = null 
         this.selected = {}
-        this.videoDuration = 0;
+        this.videoDuration = 0
+
     }
 
-    initializeCodec(){
-        this.videoElement = document.getElementById("vid");
+    initializeCodec() {
+        this.videoElement = document.getElementById("vid")
         const mediaSourceSupported = !!window.MediaSource
-        if(!mediaSourceSupported){
-            alert('Seu browser ou sistema não tem suport a MSE !');
-            return
+        if(!mediaSourceSupported) {
+            alert('Seu browser ou sistema nao tem suporte a MSE!')
+            return;
         }
 
-        const codecSupport = MediaSource.isTypeSupported(this.manifestJSON.codec)
-        if(!codecSupport){
-            alert(`Seu browser não suporta o codec: ${this,this.manifestJSON.codec}`);
-            return
+        const codecSupported = MediaSource.isTypeSupported(this.manifestJSON.codec)
+        if(!codecSupported) {
+            alert(`Seu browser nao suporta o codec: ${this.manifestJSON.codec}`)
+            return;
         }
 
-
-        const mediaSource = new MediaSource();
-        this.videoElement.src = URL.createObjectURL(mediaSource);
+        const mediaSource = new MediaSource()
+        this.videoElement.src = URL.createObjectURL(mediaSource)
 
         mediaSource.addEventListener("sourceopen", this.sourceOpenWrapper(mediaSource))
     }
 
-    sourceOpenWrapper(mediaSource){
-        return async(_) =>{
-            this.sourceBuffer = mediaSource.addSourceBuffer(this.manifestJSON.codec);
-            const select  = this.selected = this.manifestJSON.intro;
-
-            mediaSource.duration = this.videoDuration;
-            await this.fileDownload(select.url)
+    sourceOpenWrapper(mediaSource) {
+        return async(_) => {
+            this.sourceBuffer = mediaSource.addSourceBuffer(this.manifestJSON.codec)
+            const selected = this.selected = this.manifestJSON.intro
+            // evita rodar como "LIVE"
+            mediaSource.duration = this.videoDuration
+            await this.fileDownload(selected.url)
         }
-
     }
 
-    async fileDownload(url){
+    async fileDownload(url) {
+        console.log('file', url);
         const prepareUrl = {
             url,
             fileResolution: 360,
             fileResolutionTag: this.manifestJSON.fileResolutionTag,
             hostTag: this.manifestJSON.hostTag
         }
-        const finalURL = this.network.parseManifestURL(prepareUrl);
-        this.setVideoPlayerDuration(finalURL)
-        const data = await this.network.fetchFile(finalURL);
+
+        const finalUrl = this.network.parseManifestURL(prepareUrl)
+        this.setVideoPlayerDuration(finalUrl)
+        console.log('final trl', finalUrl);
+        const data = await this.network.fetchFile(finalUrl)
         return this.processBufferSegments(data)
     }
 
-    setVideoPlayerDuration(finalURL){
-        const bars = finalURL.split('/');
-        const [name, videoDuration] = bars[bars.length - 1].split('-');
-        this.videoDuration += parseFloat(videoDuration)
-
+    setVideoPlayerDuration(finalURL) {
+        const bars = finalURL.split('/')
+        const [ name, videoDuration] = bars[bars.length - 1].split('-')
+        this.videoDuration += videoDuration
     }
-
-    
-
-    async processBufferSegments(allSegments){
-        const sourceBuffer = this.sourceBuffer;
+    async processBufferSegments(allSegments) {
+        const sourceBuffer = this.sourceBuffer
         sourceBuffer.appendBuffer(allSegments)
 
-        return new Promise((resolve, reject)=>{
-            const updateEnd = (_)=>{
+        return new Promise((resolve, reject) => {
+            const updateEnd = (_) => {
                 sourceBuffer.removeEventListener("updateend", updateEnd)
-                sourceBuffer.timestampOffset = this.videoDuration;
+                sourceBuffer.timestampOffset = this.videoDuration
 
                 return resolve()
             }
